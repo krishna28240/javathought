0d1a608a36fc7613f1f708f7705b0668cc4b795c
Krishna Chavan <krishnachavan1145@gmail.com>
2020-05-09 16:36:00 +0530

    spring cloud config server

---
+++ spring-boot/spring-cloud-config-server/src/test/java/io/javathought/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javathought.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
