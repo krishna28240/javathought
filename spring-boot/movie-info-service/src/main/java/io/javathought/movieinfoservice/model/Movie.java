91a4038307c218bff12073e4d91f238316597df5
Krishna Chavan <krishnachavan1145@gmail.com>
2020-05-09 16:29:27 +0530

    movie info service

---
+++ spring-boot/movie-info-service/src/main/java/io/javathought/movieinfoservice/model/Movie.java
@@ -0,0 +1,37 @@
+package io.javathought.movieinfoservice.model;
+
+public class Movie {
+	private String movieID;
+	private String title;
+	private String overview;
+	public Movie() {
+		
+	}
+	
+	public Movie(String movieID, String title, String overview) {
+		this.movieID = movieID;
+		this.title = title;
+		this.overview = overview;
+	}
+
+	public String getMovieID() {
+		return movieID;
+	}
+	public void setMovieID(String movieID) {
+		this.movieID = movieID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getOverview() {
+		return overview;
+	}
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+	
+	
+}
